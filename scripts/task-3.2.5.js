d7415e44323f366d302faab62d2ba3f0a1df52e3
RussBow <72761639+RussBow@users.noreply.github.com>
2020-11-10 21:07:06 +0000

    Create task-3.2.5.js

---
+++ scripts/task-3.2.5.js
@@ -0,0 +1,13 @@
+var temperature =60
+
+if (temperature < 0) {
+    console.log('Stay indoors');
+} else if (temperature < 30) {
+    console.log('put a coat and hat on');
+} else if (temperature < 50) {
+    console.log('Put a coat on');
+} else {
+    console.log('just pants and vest');
+}
+    
+
