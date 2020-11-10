74c2ff04161de680d31676482b2b6371e8fa7cad
RussBow <72761639+RussBow@users.noreply.github.com>
2020-11-10 20:16:13 +0000

    task-3.2.3

---
+++ scripts/task-3.2.3.js
@@ -0,0 +1,10 @@
+function fullName(firstName, lastName) {
+
+    return firstName + lastName;
+}
+
+console.log(fullName('Russ', 'Bowen'));
+
+var completeName = fullName('Russ', 'Bowen');
+
+document.write(completeName);
