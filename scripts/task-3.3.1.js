f18e9b38f8d0fa1acdd66cd84e953690cb16c7c1
RussBow <72761639+RussBow@users.noreply.github.com>
2020-11-12 19:48:49 +0000

    Create task-3.3.1.js

---
+++ scripts/task-3.3.1.js
@@ -0,0 +1,9 @@
+function percentageCalculator(number, percentage){
+    console.log(number / 100 * percentage)
+
+var percentageOf = ( number * percentage ) / 100;
+
+return percentageOf;
+}
+
+console.log(percentageCalculator (100, 25) );
