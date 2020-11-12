211c16f95f18748d218eca25a745ecbb42d21339
RussBow <72761639+RussBow@users.noreply.github.com>
2020-11-12 19:27:56 +0000

    task 3.2.6 update

---
+++ scripts/task-3.2.6.js
@@ -0,0 +1,16 @@
+var temperature =60
+
+if (temperature < 0) {
+    console.log('Stay indoors');
+
+} else if (temperature < 30) {
+    console.log('put a coat and hat on');
+} else if (temperature < 50) {
+    console.log('Put a coat on');
+} else if ( temperature >50 && temperature === 55) { 
+    console/log('its very hote, looks like you will burn');
+} else {
+    console.log('just pants and vest');
+}
+
+//logical operators && (AND) || (OR) ! (NOT)
