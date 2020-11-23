2a6c8192ae5b32b16ee3d0d441b38cacb9157987
RussBow <72761639+RussBow@users.noreply.github.com>
2020-11-23 20:32:24 +0000

    Create task-3.4.3.1.js

---
+++ scripts/task-3.4.3.1.js
@@ -0,0 +1,9 @@
+var recipe = {
+
+    recipeTitle: 'Cottage Pie',
+    Servings: 10,
+    ingredients:  ['beef mince', '2 Onions', '3 carrots'],
+    method: 'Step 1 Heat 1 tbsp olive oil in a large saucepan and fry 1¼ kg beef mince until browned – you may need to do this in batches. Set aside as it browns.'
+}
+
+console.log(recipe)
