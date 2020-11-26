212776053720bb0550d3feb754f998682b4ba708
RussBow <72761639+RussBow@users.noreply.github.com>
2020-11-26 20:57:44 +0000

    Create task-3.7.1.js

---
+++ scripts/task-3.7.1.js
@@ -0,0 +1,43 @@
+const listItems = document.querySelectorAll('button.selection');
+
+const allimages = document.querySelectorAll('.main .image-selections .images');
+
+
+function toggleActiveClass( active ){
+
+    listItems.forEach( function( item) {
+        item.classList.remove('active');
+
+
+    } )
+
+    active.classList.add('active');
+}
+
+function toggleimages( dataClass ){
+
+    if( dataClass === 'all' ){
+
+        allimages.forEach( function( image ) {
+
+            image.style.display = 'block';
+
+        })
+    } else {
+        allimages.forEach( image => { image.dataset.class === dataClass ? image.style.display = 'block' : image.style.display = 'none'; })
+
+    }
+
+} //END OF TOGGLE IMAGES
+
+
+listItems.forEach( function( item ) {
+
+    item.addEventListener('click', function() {
+
+        toggleActiveClass( item );
+        toggleimages( item.dataset.class );
+
+    })
+
+})
